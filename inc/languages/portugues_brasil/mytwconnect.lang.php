66e5a5e0416b74271a1a2691a8c7429c6a722559
dthiago <thiagoalves@live.com>
2013-04-23 00:58:38 -0300

    Create mytwconnect.lang.php

---
+++ inc/languages/portugues_brasil/mytwconnect.lang.php
@@ -0,0 +1,43 @@
+<?php
+
+$l['mytwconnect'] = "Conexão MyTwitter";
+$l['mytwconnect_login'] = "Acesso via Twitter";
+
+// redirects
+$l['mytwconnect_redirect_loggedin'] = "Você acessou corretamente com o Twitter";
+$l['mytwconnect_redirect_registered'] = "Você registrou e acessou corretamente com o Twitter";
+$l['mytwconnect_redirect_title'] = "Olá, {1}!";
+
+// errors
+$l['mytwconnect_error_noconfigfound'] = "YVocê não configurou MyTwitter plug-in ainda: ou o ID do aplicativo do Twitter ou o seu token secreto do Twitter estão faltando. Se você for um administrador, por favor leia as instruções fornecidas na documentação.";
+$l['mytwconnect_error_noauth'] = "Você não nos deixou fazer o login com sua conta do Twitter. Por favor, autorizar nosso derente de aplicativos do Twitter se você deseja  fazer login em nosso Fórum.";
+$l['mytwconnect_error_alreadyloggedin'] = "Você está logado neste fórum.";
+$l['mytwconnect_error_report'] = "Um erro desconhecido com o servidor de API do Twitter remoto ocorreu. A saída do erro está:
+
+{1}
+
+If you don't know what it means, report it to an administrator.";
+
+// usercp
+$l['mytwconnect_settings_title'] = "Integração Twitter";
+$l['mytwconnect_settings_save'] = "Salve";
+$l['mytwconnect_settings_twavatar'] = "Avatar and cover";
+$l['mytwconnect_settings_twbio'] = "Bio";
+$l['mytwconnect_settings_twlocation'] = "Localidade";
+$l['mytwconnect_link'] = "Clique para anexar sua conta ao Twitter";
+$l['mytwconnect_settings_whattosync'] = "Selecione a informação que deves importar a partir do seu Twitter. Vamos sincronizar imediatamente seus dados desejados e atualizar as configurações, acrescentando o que deve ser adicionado (mas não removendo o que deveria ser removido - que depende de você!).";
+$l['mytwconnect_settings_linkaccount'] = "Marque a opção em seu lado direito para vincular a sua conta do Twitter com a sua neste fórum.";
+
+// registration
+$l['mytwconnect_register_title'] = "Inscrição Twitter";
+$l['mytwconnect_register_basicinfo'] = "Escolha as infos básicas à sua direita. Eles já estão preenchidas com seus dados do Twitter, mas se você quiser mudá-las você é livre para fazê-lo. A conta será vinculado ao seu Twitter imediatamente, de forma automática e independentemente de suas escolhas..";
+$l['mytwconnect_register_whattosync'] = "Selecione o que deseja importar do Twitter. Dependendo de suas escolhas iremos sincronizar imediatamente os dados desejados de sua conta do Twitter.";
+$l['mytwconnect_register_username'] = "Nome de Usuário:";
+$l['mytwconnect_register_email'] = "Email:";
+$l['mytwconnect_register_username_placeholder'] = "Especifique seu nome de usuário";
+$l['mytwconnect_register_email_placeholder'] = "Especifique o seu email aqui";
+
+// success messages
+$l['mytwconnect_success_linked'] = "A sua conta do fórum foi corretamente anexada ao Twitter.";
+$l['mytwconnect_success_settingsupdated'] = "As suas configurações relacionadas com integração com o Twitter foram atualizados corretamente.";
+$l['mytwconnect_success_settingsupdated_title'] = "Configurações atualizadas";
